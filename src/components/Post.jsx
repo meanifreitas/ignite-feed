df477969991100e4cc775c308f88613dc015c9af
Meani Freitas <meani.sf@gmail.com>
2024-01-05 14:34:04 -0300

    Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/meanifreitas.png" className={styles.avatar} />
+          <Avatar src="https://github.com/meanifreitas.png" />
           <div className={styles.authorInfo}>
             <strong>Meani Freitas</strong>
             <span>Web developer</span>
